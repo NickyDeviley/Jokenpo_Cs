b9d9684153cf48f2015935c139763a67b540e195
NickyDeviley <Nickmilanin@gmail.com>
2025-08-23 01:30:55 -0300

    Adicionar arquivos de projeto.

---
+++ Jokenpo/main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo
+{
+    public class main
+    {
+
+        public static Random sorteador = new Random();
+
+        public static void Main(String[] args)
+        {
+
+            // Jokenpo - Jogo do Pedra, Papel e Tesoura
+
+            int escolhaUsuario = 0;
+
+            Console.WriteLine("Bem-vindo ao Jokenpo!");
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Pedra \n2 = Papel \n3 - Tesoura");
+
+            escolhaUsuario = Convert.ToInt32(Console.ReadLine());
+
+            if (escolhaUsuario < 1 || escolhaUsuario > 3)
+            {
+                Console.WriteLine("Opção inválida. Tente novamente.");
+                return;
+            }
+
+            int escolhaComputador = sorteador.Next(1, 4); // Gera um número aleatório entre 1 e 3
+
+            if (escolhaUsuario == escolhaComputador)
+            {
+                Console.WriteLine("Empate! Ambos escolheram a mesma opção.");
+            }
+            else
+            {
+
+                if (escolhaComputador == 1 && escolhaUsuario == 2)
+                {
+                    metodos.mensagemV(escolhaComputador, escolhaUsuario);
+                }
+                else if (escolhaComputador == 2 && escolhaUsuario == 3)
+                {
+                    metodos.mensagemV(escolhaComputador, escolhaUsuario);
+                }
+                else if (escolhaComputador == 3 && escolhaUsuario == 1)
+                {
+                    metodos.mensagemV(escolhaComputador, escolhaUsuario);
+                }
+                else if (escolhaComputador == 1 && escolhaUsuario == 3)
+                {
+                    metodos.mensagemD(escolhaComputador, escolhaUsuario);
+                }
+                else if (escolhaComputador == 2 && escolhaUsuario == 1)
+                {
+                    metodos.mensagemD(escolhaComputador, escolhaUsuario);
+                }
+                else if (escolhaComputador == 3 && escolhaUsuario == 2)
+                {
+                    metodos.mensagemD(escolhaComputador, escolhaUsuario);
+
+                }
+            }
+        }
+    }
+}
---
+++ Jokenpo/metodos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo
+{
+    internal class metodos
+    {
+        public static void mensagemV(int escolhaComputador, int escolhaUsuario)
+        {
+            Console.WriteLine("Você venceu!");
+            Console.WriteLine("Computador escolheu: " + mensagemE(escolhaComputador));
+            Console.WriteLine("Você escolheu: " + mensagemU(escolhaUsuario));
+        }
+        public static void mensagemD(int escolhaComputador, int escolhaUsuario)
+        {
+            Console.WriteLine("Você perdeu!");
+            Console.WriteLine("Computador escolheu: " + mensagemE(escolhaComputador));
+            Console.WriteLine("Você escolheu: " + mensagemU(escolhaUsuario));
+        }
+
+        public static string mensagemE(int escolhaComputador)
+        {
+            string escolhaC = "";
+
+            if (escolhaComputador == 1)
+            {
+                escolhaC = "Pedra";
+            }
+            else if (escolhaComputador == 2)
+            {
+                escolhaC = "Papel";
+            }
+            else if (escolhaComputador == 3)
+            {
+                escolhaC = "Tesoura";
+            }
+            return escolhaC;
+        }
+        public static string mensagemU(int escolhaUsuario)
+        {
+            string escolhaU = "";
+            if (escolhaUsuario == 1)
+            {
+                escolhaU = "Pedra";
+            }
+            else if (escolhaUsuario == 2)
+            {
+                escolhaU = "Papel";
+            }
+            else if (escolhaUsuario == 3)
+            {
+                escolhaU = "Tesoura";
+            }
+            return escolhaU;
+        }
+
+    }
+}
